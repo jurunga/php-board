c09d920ff3833c8f0aa4893c7f6541531513dd18
jurunga <jurunga@users.noreply.github.com>
2021-01-11 12:49:30 +0900

    Create login.php

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div algin=center>
+    <span>로그인</span>
+
+    <form method='get' action='login_action.php'>
+        <p>ID:<input name="id" type="text">
+        <p>PW:<input name="pw" type="password"></p>
+        <input type="submit" value="로그인">
+    </form>
+    <br/>
+    <button id="">
+    </button>
+</body>
+</html>
