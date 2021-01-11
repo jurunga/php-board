b614528e90a8d985e03ad4c76d691b357902b195
jurunga <jurunga@users.noreply.github.com>
2021-01-11 13:59:07 +0900

    Create join.php

---
+++ join.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div align="center">
+    <p>회원가입</p>
+        <form method='get' action='join_action.php'>
+            <p>ID: <input type="text" name="id"></p>
+            <p>PW: <input type="password" name="pw"></p>
+            <p>Email: <input type="text" name="email"></p>
+            <intput type="submit" value="회원가입">
+            </form>
+        </div>
+</body>
+</html>
