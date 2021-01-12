083c66239a4a771e04d0ca11485c7b7ed9cdce21
jurunga <jurunga@users.noreply.github.com>
2021-01-12 10:41:21 +0900

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,12 @@
+<?php
+    $connect = mysqli_connect("localhost", "root", "", "board") or die("connect fail");
+
+    $number = $_GET['number'];
+    $id = $_GET['userid'];
+    $query = "delete from board where number = '$number'";
+    $result = $connect->query($query);
+?>
+
+<script>alert("삭제되었습니다.");
+    location.replace("./index.php");
+</script>
